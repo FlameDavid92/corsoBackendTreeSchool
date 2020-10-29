f9c195674a097f7b4873cee9b7c6d22f3533fb13
FlameDavid92 <davidefiguccia2020@outlook.it>
2020-10-29 10:01:01 +0100

    listaOrdinataNuovoRemove

--- esercizi7/listaordinata/ListaOrdinata.java
+++ esercizi7/listaordinata/ListaOrdinata.java
@@ -1,23 +1,15 @@
 package it.corsobackendtree.esercizi7.listaordinata;
 
-import java.util.Arrays;
-
 public class ListaOrdinata {
     private Integer[] arrayOrdinato;
     private int lastFreeIndex;
-    private int cancellazioni;
 
     public ListaOrdinata() {
         this.arrayOrdinato = new Integer[5];
         this.lastFreeIndex = 0;
-        this.cancellazioni = 0;
     }
 
     public void insert(int x) {
-        /*Ogni 10 cancellazioni elimino i possibili null interni dovuti alle cancellazioni.*/
-        if (this.cancellazioni == 10) {
-            clean();
-        }
         /*Se non ho più spazio nell'array per l'inserimento, raddoppio lo spazio disponibile.*/
         if (this.lastFreeIndex == this.arrayOrdinato.length - 1) {
             doubleSpace();
@@ -52,9 +44,6 @@ public class ListaOrdinata {
 
     public int indexOf(int x) {
         /*Eseguo un clean così non ho null interni.*/
-        if (this.cancellazioni != 0) {
-            clean();
-        }
         return ricercaBinaria(this.arrayOrdinato,
                 0, this.lastFreeIndex - 1, x);
     }
@@ -72,40 +61,26 @@ public class ListaOrdinata {
     }
 
     public void remove(int x) {
-        for (int i = 0; i < lastFreeIndex; i++) {
-            if (this.arrayOrdinato[i] == null) {
-                continue;
+        int index = indexOf(x);
+        if (index < lastFreeIndex-1) {
+            this.arrayOrdinato[index] = this.arrayOrdinato[index + 1];
+            for (int i = index + 1; i < lastFreeIndex; i++) {
+                if (i == lastFreeIndex - 1) this.arrayOrdinato[i] = null;
+                else {
+                    this.arrayOrdinato[i] = this.arrayOrdinato[i + 1];
+                }
             }
-            if (this.arrayOrdinato[i] == x) {
-                this.arrayOrdinato[i] = null;
-            }
-        }
-        this.cancellazioni++;
+        }else this.arrayOrdinato[lastFreeIndex-1] = null;
+        lastFreeIndex--;
     }
 
     public void print() {
         System.out.print("[");
         for (int i = 0; i < lastFreeIndex; i++) {
-            if (arrayOrdinato[i] == null) continue;
-
             if (i == lastFreeIndex - 1) System.out.print(arrayOrdinato[i]);
             else System.out.print(arrayOrdinato[i] + ",");
         }
         System.out.print("]\n");
-    }
-
-    public void clean() {
-        int length = this.arrayOrdinato.length;
-        Integer[] newArrayOrdinato = new Integer[length];
-        int j = 0;
-        for (int i = 0; i < this.lastFreeIndex; i++) {
-            if (this.arrayOrdinato[i] != null) {
-                newArrayOrdinato[j] = this.arrayOrdinato[i];
-                j++;
-            }
-        }
-        this.arrayOrdinato = newArrayOrdinato;
-        this.lastFreeIndex = j;
     }
 
     public void doubleSpace() {
