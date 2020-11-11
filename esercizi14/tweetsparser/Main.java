cadf493142cd15e37658c4806684153d7c4e6175
FlameDavid92 <davidefiguccia2020@outlook.it>
2020-11-11 14:24:49 +0100

    superTweetsParser

--- esercizi14/tweetsparser/Main.java
+++ esercizi14/tweetsparser/Main.java
@@ -1,19 +1,38 @@
 package it.corsobackendtree.esercizi14.tweetsparser;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) {
         TweetsParser tp = new TweetsParser();
-        TreeMap<Long,String> mostFrequent10 = tp.mostFrequent10Words("./it/corsobackendtree/esercizi14/tweetsparser/realdonaldtrump.csv");
+        TreeMap<Long, List<String>> mostFrequent = tp.mostFrequent10Words("./it/corsobackendtree/esercizi14/tweetsparser/realdonaldtrump.csv");
+        TreeMap<Long, List<String>> leastFrequent = tp.leastFrequentWords("./it/corsobackendtree/esercizi14/tweetsparser/realdonaldtrump.csv");
+
+        System.out.println("MOST FREQUENT - realdonaldtrump.csv (numOccurences=[words])");
         for (int i = 0; i < 10; i++) {
-            System.out.println(mostFrequent10.pollLastEntry());
+            System.out.println(mostFrequent.pollLastEntry());
         }
         System.out.println("------");
-
-        mostFrequent10 = tp.mostFrequent10Words("./it/corsobackendtree/esercizi14/tweetsparser/trumptweets.csv");
+        System.out.println("LEAST FREQUENT - realdonaldtrump.csv (numOccurences=[words])");
         for (int i = 0; i < 10; i++) {
-            System.out.println(mostFrequent10.pollLastEntry());
+            System.out.println(leastFrequent.pollFirstEntry());
+        }
+
+        System.out.println("------\n------");
+
+        mostFrequent = tp.mostFrequent10Words("./it/corsobackendtree/esercizi14/tweetsparser/trumptweets.csv");
+        leastFrequent = tp.leastFrequentWords("./it/corsobackendtree/esercizi14/tweetsparser/trumptweets.csv");
+
+        System.out.println("MOST FREQUENT - trumptweets.csv (numOccurences=[words])");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(mostFrequent.pollLastEntry());
+        }
+        System.out.println("------");
+        System.out.println("LEAST FREQUENT - trumptweets.csv (numOccurences=[words])");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(leastFrequent.pollFirstEntry());
         }
     }
 }
--- esercizi14/tweetsparser/TweetsParser.java
+++ esercizi14/tweetsparser/TweetsParser.java
@@ -12,27 +12,83 @@ public class TweetsParser {
     Map<String, Long> wordsOccurrences;
     String regex;
     Pattern pattern;
+    String lastPathComputed;
 
     public TweetsParser(){
+        lastPathComputed = "";
         regex = "[^\\d\\W]+"; /*all words with no digits*/
         pattern = Pattern.compile(regex);
     }
 
-    public TreeMap<Long, String> mostFrequent10Words(String csvFilePath) {
+    public TreeMap<Long, List<String>> mostFrequent10Words(String csvFilePath) {
+        computeWordsOccurences(csvFilePath);
+        TreeMap<Long, List<String>> mostFrequent10 = new TreeMap<>();
+        for (String key : wordsOccurrences.keySet()) {
+            Long occurences = wordsOccurrences.get(key);
+
+            if (mostFrequent10.size() <= 10) {
+                if(mostFrequent10.containsKey(occurences)) mostFrequent10.get(occurences).add(key);
+                else{
+                    LinkedList<String> parole = new LinkedList<>();
+                    parole.add(key);
+                    mostFrequent10.put(occurences, parole);
+                }
+            } else {
+                if (wordsOccurrences.get(key) > mostFrequent10.firstKey()) {
+                    mostFrequent10.pollFirstEntry();
+                    LinkedList<String> parole = new LinkedList<>();
+                    parole.add(key);
+                    mostFrequent10.put(occurences, parole);
+                }else if(wordsOccurrences.get(key) == mostFrequent10.firstKey()){
+                    mostFrequent10.get(occurences).add(key);
+                }
+            }
+        }
+        return mostFrequent10;
+    }
+
+    public TreeMap<Long, List<String>> leastFrequentWords(String csvFilePath) {
+        computeWordsOccurences(csvFilePath);
+        TreeMap<Long, List<String>> leastFrequent10 = new TreeMap<>();
+        for (String key : wordsOccurrences.keySet()) {
+            Long occurences = wordsOccurrences.get(key);
+
+            if (leastFrequent10.size() <= 10) {
+                if(leastFrequent10.containsKey(occurences)) leastFrequent10.get(occurences).add(key);
+                else{
+                    LinkedList<String> parole = new LinkedList<>();
+                    parole.add(key);
+                    leastFrequent10.put(occurences, parole);
+                }
+            } else {
+                if (wordsOccurrences.get(key) < leastFrequent10.lastKey()) {
+                    leastFrequent10.pollLastEntry();
+                    LinkedList<String> parole = new LinkedList<>();
+                    parole.add(key);
+                    leastFrequent10.put(occurences, parole);
+                }else if(wordsOccurrences.get(key) == leastFrequent10.lastKey()){
+                    leastFrequent10.get(occurences).add(key);
+                }
+            }
+        }
+        return leastFrequent10;
+    }
+
+    private void computeWordsOccurences(String csvFilePath){
+        if(csvFilePath.equals(lastPathComputed)) return;
+
         wordsOccurrences = new HashMap<>();
         try (BufferedReader br = new BufferedReader(new FileReader(csvFilePath))) {
             String line = br.readLine(); /*salta la prima riga*/
             while ((line = br.readLine()) != null) {
                 String content = CSVUtils.parseLine(line).get(2);
-                if (!content.contains("pic.twitter") && !content.contains("http")) {
-                    Matcher matcher = pattern.matcher(line);
-                    while(matcher.find()) {
-                        String word = matcher.group();
-                        if (wordsOccurrences.containsKey(word)) {
-                            wordsOccurrences.put(word, wordsOccurrences.get(word) + 1);
-                        } else {
-                            wordsOccurrences.put(word, 1L);
-                        }
+                Matcher matcher = pattern.matcher(line);
+                while(matcher.find()) {
+                    String word = matcher.group();
+                    if (wordsOccurrences.containsKey(word)) {
+                        wordsOccurrences.put(word, wordsOccurrences.get(word) + 1);
+                    } else {
+                        wordsOccurrences.put(word, 1L);
                     }
                 }
             }
@@ -41,18 +97,5 @@ public class TweetsParser {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        TreeMap<Long, String> mostFrequent10 = new TreeMap<>();
-        for (String key : wordsOccurrences.keySet()) {
-            if (mostFrequent10.size() < 10) {
-                mostFrequent10.put(wordsOccurrences.get(key), key);
-            } else {
-                if (wordsOccurrences.get(key) > mostFrequent10.firstKey()) {
-                    mostFrequent10.pollFirstEntry();
-                    mostFrequent10.put(wordsOccurrences.get(key), key);
-                }
-            }
-        }
-        return mostFrequent10;
     }
 }
