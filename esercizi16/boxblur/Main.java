7c05d270214ae9e5a9ff83958705fee4501b6431
FlameDavid92 <davidefiguccia2020@outlook.it>
2020-11-20 13:07:45 +0100

    boxBlur++

--- esercizi16/boxblur/Main.java
+++ esercizi16/boxblur/Main.java
@@ -4,7 +4,6 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
