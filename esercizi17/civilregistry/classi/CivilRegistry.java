be8f97105316097cf0f13eeba2ab44f5ab342118
FlameDavid92 <davidefiguccia2020@outlook.it>
2020-11-23 15:00:33 +0100

    civilRegistryParallel

--- esercizi17/civilregistry/classi/CivilRegistry.java
+++ esercizi17/civilregistry/classi/CivilRegistry.java
@@ -1,9 +1,6 @@
 package it.corsobackendtree.esercizi17.civilregistry.classi;
 
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -17,38 +14,40 @@ public class CivilRegistry {
     }
 
     private CivilRegistry(){
-        persone = new HashSet<>();
+        persone = Collections.synchronizedSet(new HashSet<>());
     }
 
     public void inserisciPersona(Persona p){
-        if(!persone.contains(p)) persone.add(p);
+        if(!persone.contains(p)){
+            persone.add(p);
+        }
         else System.out.println("Persona già presente!");
     }
 
     public void rimuoviPersona(CodiceFiscaleItaliano codiceFiscale){
-        persone = persone.stream().filter(p -> !p.getCodiceFiscale().equals(codiceFiscale)).collect(Collectors.toSet());
+        persone = persone.stream().parallel().filter(p -> !p.getCodiceFiscale().equals(codiceFiscale)).collect(Collectors.toSet());
     }
 
     public List<Persona> getPersone(String nomePersona){
-        return getStreamAfterNameFilter(nomePersona).collect(Collectors.toList());
+        return getParallelStreamAfterNameFilter(nomePersona).collect(Collectors.toList());
     }
 
     public List<Persona> getTrePiuAnziani(){
-        return persone.stream().sorted(Comparator.comparingInt(Persona::getEta).reversed()).limit(3).collect(Collectors.toList());
+        return persone.stream().parallel().sorted(Comparator.comparingInt(Persona::getEta).reversed()).limit(3).collect(Collectors.toList());
     }
 
     public List<Indirizzo> getIndirizzi(String nomePersona){
-        return getStreamAfterNameFilter(nomePersona)
+        return getParallelStreamAfterNameFilter(nomePersona)
                 .map(Persona::getResidenza).collect(Collectors.toList());
     }
 
     public List<Persona> getFigli(String nomePersona){
-        return getStreamAfterNameFilter(nomePersona)
+        return getParallelStreamAfterNameFilter(nomePersona)
                 .flatMap(e->e.figli.stream()).collect(Collectors.toList());
     }
 
-    private Stream<Persona> getStreamAfterNameFilter(String nomePersona){
+    private Stream<Persona> getParallelStreamAfterNameFilter(String nomePersona){
         String lowerCaseName = nomePersona.toLowerCase().trim();
-        return persone.stream().filter(p -> p.getNome().toLowerCase().startsWith(lowerCaseName));
+        return persone.stream().parallel().filter(p -> p.getNome().toLowerCase().startsWith(lowerCaseName));
     }
 }
